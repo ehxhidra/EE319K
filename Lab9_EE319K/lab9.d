6b1eb541b283d65065fb2306466a79b81eb782fb
ehxhidra <hailhidramk1@gmail.com>
2023-04-10 02:21:21 -0500

    updates

--- Lab9_EE319K/lab9.d
+++ Lab9_EE319K/lab9.d
@@ -1,14 +1,5 @@
 ./lab9.o: Lab9.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
   ..\inc\ST7735.h TExaS.h ..\inc\ADC.h ..\inc\tm4c123gh6pm.h UART1.h \
-<<<<<<< HEAD
-  C:\Users\yunja\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\tm4c123gh6pm.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\yunja\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
-=======
   C:\Users\joshu\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\tm4c123gh6pm.h \
   C:\Users\joshu\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
   C:\Users\joshu\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
@@ -16,5 +7,4 @@
   C:\Users\joshu\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
   C:\Users\joshu\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
   C:\Users\joshu\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
->>>>>>> 995cab1b3f88fae909b50095171ae5f6b70d56ce
   Fifo.h ..\inc\StringConversion.h
