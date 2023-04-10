675d09e1408c4aceec9fe02dc81f2a27e0f5c8d8
ehxhidra <hailhidramk1@gmail.com>
2023-04-10 01:35:50 -0500

    update

--- Lab9_EE319K/Lab9.c
+++ Lab9_EE319K/Lab9.c
@@ -130,7 +130,7 @@ int main(void){
   }
 }
 
-char msg[5]; 
+char mesg[5]; 
 void Timer3A_Handler(void){
   TIMER3_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER2A timeout
   // write this
@@ -143,14 +143,14 @@ int smp = ADC_In();
 GPIO_PORTF_DATA_R ^= 0x02;
 //4) convert to distance and create the 8-byte message
 int distance = Convert(smp);
-Fix2String(smp, msg);
+Fix2String(smp, mesg);
 //5) send the 8-byte message to the other computer (calls UART1_OutChar 8 times)
 UART1_OutChar(0x3C); // <
-UART1_OutChar(msg[0]);
-UART1_OutChar(msg[1]);
-UART1_OutChar(msg[2]);
-UART1_OutChar(msg[3]);
-UART1_OutChar(msg[4]);
+UART1_OutChar(mesg[0]);
+UART1_OutChar(mesg[1]);
+UART1_OutChar(mesg[2]);
+UART1_OutChar(mesg[3]);
+UART1_OutChar(mesg[4]);
 UART1_OutChar(0x3E);  // >
 UART1_OutChar(0x0A); // LF
 //6) increment a TxCounter, used as debugging monitor of the number of ADC samples collected 
